fff72112b77de2f3190f2f7b0bf9e5ac9170d95a
Lukas Bischof <lukas.bischof@renuo.ch>
2019-05-06 10:18:05 +0200

    Implement reverse lookup

--- api/app/services/service_calculator.rb
+++ api/app/services/service_calculator.rb
@@ -21,33 +21,45 @@ class ServiceCalculator
 
   LINEAR_CALCULATION_THRESHOLD = 26
   CONVERTED_LOOKUP_TABLE = [DAY_LOOKUP_TABLE.keys.flatten, DAY_LOOKUP_TABLE.values.flatten].transpose.to_h.freeze
+  REVERSED_CONVERTED_LOOKUP_TABLE = CONVERTED_LOOKUP_TABLE.reverse_each.to_h.freeze
 
   def initialize(beginning_date)
     @beginning_date = beginning_date
   end
 
   def calculate_ending_date(required_service_days)
+    raise I18n.t('service_calculator.invalid_required_service_days') unless required_service_days.positive?
+
     return calculate_linear_ending_date(required_service_days) if required_service_days >= LINEAR_CALCULATION_THRESHOLD
 
     calculate_irregular_ending_date(required_service_days)
   end
 
   def calculate_chargeable_service_days(ending_date)
+    raise I18n.t('service_calculator.end_date_cannot_be_on_weekend') if ending_date.on_weekend?
 
+    duration = (ending_date - @beginning_date).to_i + 1
+    return duration if duration >= LINEAR_CALCULATION_THRESHOLD
+
+    reverse_duration_lookup(duration)
   end
 
   private
 
   def calculate_irregular_ending_date(required_service_days)
-    @beginning_date + duration_lookup(required_service_days).days
+    @beginning_date + duration_lookup(required_service_days).days - 1.day
   end
 
   def duration_lookup(required_service_days)
     CONVERTED_LOOKUP_TABLE[required_service_days]
   end
 
+  def reverse_duration_lookup(service_duration)
+    REVERSED_CONVERTED_LOOKUP_TABLE.key service_duration
+  end
+
   def calculate_linear_ending_date(required_service_days)
-    ending = @beginning_date + required_service_days.days
+    ending = @beginning_date + required_service_days.days - 1.day
     return ending unless ending.on_weekend?
 
     (ending + 1.week).at_beginning_of_week
--- api/spec/support/service_calculator_spec_helper.rb
+++ api/spec/support/service_calculator_spec_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples_for 'adds one day to linear duration' do |range|
-  it 'returns correct eligible days', :aggregate_failures do
+  it 'returns correct ending date', :aggregate_failures do
     range.each do |delta|
       ending = ServiceCalculator.new(beginning).calculate_ending_date(delta)
-      expect((ending - beginning).to_i).to eq(delta + 1)
+      expect((ending - beginning).to_i).to eq(delta)
     end
   end
 end
