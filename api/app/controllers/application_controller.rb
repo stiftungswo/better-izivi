f113a0cda68d10d47a001b311695625288f38d4c
Lukas Bischof <lukas.bischof@renuo.ch>
2019-05-06 17:45:37 +0200

    Configuration and Remove ActiveStorage

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -4,21 +4,17 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include Concerns::ErrorHandler
 
+  PERMITTED_USER_KEYS = %i[
+    regional_center_id first_name last_name
+    address bank_iban birthday city
+    health_insurance zip hometown phone zdp
+  ].freeze
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   respond_to :json
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: permitted_user_keys)
-  end
-
-  private
-
-  def permitted_user_keys
-    %i[
-      regional_center_id first_name last_name
-      address bank_iban birthday city
-      health_insurance zip hometown phone zdp
-    ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: PERMITTED_USER_KEYS)
   end
 end
--- api/config/initializers/devise.rb
+++ api/config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@stiftungswo.ch'
+  config.mailer_sender = ENV['MAIL_SENDER']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -88,7 +88,7 @@ Devise.setup do |config|
   # It will change confirmation, password recovery and other workflows
   # to behave the same regardless if the e-mail provided was right or wrong.
   # Does not affect registerable.
-  # config.paranoid = true
+  config.paranoid = true
 
   # By default Devise will store the user in session. You can skip storage for
   # particular strategies by setting this option.
