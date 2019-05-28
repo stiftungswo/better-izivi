0ac3ec1a069590dc83b022cac44f2a19ee729491
Philipp Fehr <philipp@thefehr.me>
2019-05-28 11:53:13 +0200

    Add Pdf generation

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :v1 do
+    get 'phone_list/index'
+  end
+
   namespace :v1, defaults: { format: :json } do
     resources :regional_centers, only: :index
     resources :holidays, only: %i[index create update destroy]
