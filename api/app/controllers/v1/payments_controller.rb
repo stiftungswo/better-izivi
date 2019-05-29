6c6154fa8d4c31354eca0a41ae46bba1f6db23a1
Philipp Fehr <philipp@thefehr.me>
2019-05-29 10:39:16 +0200

    Fix linting

--- api/app/controllers/v1/payments_controller.rb
+++ api/app/controllers/v1/payments_controller.rb
@@ -13,28 +13,36 @@ module V1
       sct = setup_sct
 
       sheets.each do |sheet|
-        sct.add_transaction(
-          name: sheet.user.full_name,
-          iban: sheet.user.bank_iban,
-          amount: sheet.full_amount,
-          currency: 'CHF',
-          remittance_information: I18n.t('payment.expenses_from', from_date: I18n.l(sheet.beginning, format: '%B %Y')),
-          requested_date: Time.zone.today,
-          batch_booking: true,
-
-          creditor_address: SEPA::CreditorAddress.new(
-            country_code: 'CH',
-            street_name: 'Mustergasse 123a',
-            post_code: '1234',
-            town_name: 'Musterstadt'
-          )
-        )
+        sct.add_transaction(build_transaction(sheet))
       end
 
       sct
     end
 
+    def build_transaction(sheet)
+      {
+        name: sheet.user.full_name,
+        iban: sheet.user.bank_iban,
+        amount: sheet.full_amount,
+        currency: 'CHF',
+        remittance_information: I18n.t('payment.expenses_from', from_date: I18n.l(sheet.beginning, format: '%B %Y')),
+        requested_date: Time.zone.today,
+        batch_booking: true,
+
+        creditor_address: build_creditor_address(sheet.user)
+      }
+    end
+
     # "Spesen #{I18n.l(Time.now, format: '%B')}"
+
+    def build_creditor_address(user)
+      SEPA::CreditorAddress.new(
+        country_code: 'CH',
+        street_name: user.address,
+        post_code: user.zip,
+        town_name: user.city
+      )
+    end
 
     def setup_sct
       SEPA::CreditTransfer.new(
--- api/spec/requests/v1/payments_controller_spec.rb
+++ api/spec/requests/v1/payments_controller_spec.rb
@@ -3,4 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe V1::PaymentsController, type: :request do
+
 end
