faa949819967a16c7968dda4ccca7e10e767a273
Lukas Bischof <polinderis@gmail.com>
2019-06-04 15:03:44 +0200

    Respond with XML content type

--- api/app/controllers/v1/payments_controller.rb
+++ api/app/controllers/v1/payments_controller.rb
@@ -4,7 +4,7 @@ module V1
   class PaymentsController < ApplicationController
     def export
       sheets = ExpenseSheet.includes(:user).ready_for_payment
-      render plain: PainGenerationService.new(sheets).generate_pain.to_xml('pain.001.001.03.ch.02')
+      render plain: PainGenerationService.new(sheets).generate_pain.to_xml('pain.001.001.03.ch.02'), content_type: :xml
     end
   end
 end
