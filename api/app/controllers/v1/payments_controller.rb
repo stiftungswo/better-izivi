bffbfa1c4e0adb3b0cde0d50daa3de50f4a0c074
Lukas Bischof <polinderis@gmail.com>
2019-06-04 14:57:34 +0200

    Improvements

--- api/app/controllers/v1/payments_controller.rb
+++ api/app/controllers/v1/payments_controller.rb
@@ -3,53 +3,8 @@
 module V1
   class PaymentsController < ApplicationController
     def export
-      expense_sheets = ExpenseSheet.includes(:user).ready_for_payment
-      render plain: generate_pain(expense_sheets).to_xml('pain.001.001.03.ch.02')
-    end
-
-    private
-
-    def generate_pain(sheets)
-      sepa_credit_transfer = setup_sct
-
-      sheets.each do |sheet|
-        sepa_credit_transfer.add_transaction(build_transaction(sheet))
-      end
-
-      sepa_credit_transfer
-    end
-
-    def build_transaction(sheet)
-      {
-        name: sheet.user.full_name,
-        iban: sheet.user.bank_iban,
-        amount: sheet.full_amount,
-        currency: 'CHF',
-        remittance_information: I18n.t('payment.expenses_from', from_date: I18n.l(sheet.beginning, format: '%B %Y')),
-        requested_date: Time.zone.today,
-        batch_booking: true,
-
-        creditor_address: build_creditor_address(sheet.user)
-      }
-    end
-
-    # "Spesen #{I18n.l(Time.now, format: '%B')}"
-
-    def build_creditor_address(user)
-      SEPA::CreditorAddress.new(
-        country_code: 'CH',
-        street_name: user.address,
-        post_code: user.zip,
-        town_name: user.city
-      )
-    end
-
-    def setup_sct
-      SEPA::CreditTransfer.new(
-        name: 'Lou GmbH',
-        bic: 'POFICHBEXXX',
-        iban: 'CH2409000000800040679'
-      )
+      sheets = ExpenseSheet.includes(:user).ready_for_payment
+      render plain: PainGenerationService.new(sheets).generate_pain.to_xml('pain.001.001.03.ch.02')
     end
   end
 end
--- api/spec/requests/v1/payments_controller_spec.rb
+++ api/spec/requests/v1/payments_controller_spec.rb
@@ -3,54 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe V1::PaymentsController, type: :request do
-  describe '#generate_transaction' do
-    subject(:transaction_adder) { instance_double(SEPA::CreditTransfer, add_transaction: true) }
-
-    let(:payments_controller) { V1::PaymentsController.new }
-    let(:expense_sheet) { create :expense_sheet, :ready_for_payment }
-    let(:user) { expense_sheet.user }
-
-    before do
-      allow(SEPA::CreditTransfer).to receive(:new).and_return transaction_adder
-
-      payments_controller.send(:generate_pain, [expense_sheet])
-    end
-
-    context 'when there is one expense sheet' do
-      let(:expected_fields) do
-        {
-          amount: expense_sheet.full_amount,
-          batch_booking: true,
-          currency: 'CHF',
-          iban: user.bank_iban,
-          name: user.full_name,
-          remittance_information: I18n.t('payment.expenses_from',
-                                         from_date: I18n.l(
-                                           expense_sheet.beginning,
-                                           format: '%B %Y'
-                                         )),
-          requested_date: Time.zone.today
-        }
-      end
-
-      it 'generates transaction' do
-        expect(transaction_adder).to have_received(:add_transaction)
-          .with(hash_including(expected_fields))
-      end
-    end
-  end
-
   describe '#index' do
-    subject { response }
+    let(:request) { get v1_pain_export_path }
 
-    let(:expense_sheet) { create :expense_sheet, :ready_for_payment }
+    before { create :expense_sheet, :ready_for_payment }
 
-    before do
-      expense_sheet
-
-      get v1_pain_export_path
-    end
-
-    it { is_expected.to be_successful }
+    it_behaves_like 'renders a successful http status code'
   end
 end
