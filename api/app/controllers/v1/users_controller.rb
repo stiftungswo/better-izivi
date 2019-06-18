ff8d2596536c872c3b27106e6ed98ae9bed8cc6c
Lukas Bischof <polinderis@gmail.com>
2019-06-18 08:47:55 +0200

    Protect user resource and restrict foreign access to admin only

--- api/app/controllers/v1/users_controller.rb
+++ api/app/controllers/v1/users_controller.rb
@@ -2,8 +2,21 @@
 
 module V1
   class UsersController < APIController
+    before_action :set_user
+    before_action :protect_foreign_resource!, if: -> { current_user.civil_servant? }
+
     def show
-      render partial: 'shared/users/user', locals: { user: User.find(params.require(:id)) }
+      render partial: 'shared/users/user', locals: { user: @user }
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params.require(:id))
+    end
+
+    def protect_foreign_resource!
+      raise AuthorizationError unless current_user.id == @user.id
     end
   end
 end
