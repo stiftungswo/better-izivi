0f0bf19c0edaa4ba8a7b9448d960d55b8075bde3
Lukas Bischof <polinderis@gmail.com>
2019-06-20 13:41:28 +0200

    Extract explanation header

--- frontend/src/views/users/service_subform/ServiceSubform.tsx
+++ frontend/src/views/users/service_subform/ServiceSubform.tsx
@@ -11,6 +11,7 @@ import createStyles from '../../../utilities/createStyles';
 import { serviceSchema } from '../schemas';
 import { ServiceModal } from '../ServiceModal';
 import ServiceOverviewTable from './ServiceOverviewTable';
+import { ServiceSubformExplanationHeader } from './ServiceSubformExplanationHeader';
 
 interface Props extends WithSheet<typeof styles> {
   mainStore?: MainStore;
@@ -53,18 +54,7 @@ class ServiceSubformInner extends React.Component<Props, ServiceSubformState> {
 
     return (
       <>
-        <h3>Einsatzplanung</h3>
-        <p>
-          Um eine Einsatzplanung zu erfassen, klicke unten auf "Neue Einsatzplanung hinzufügen", wähle ein Pflichtenheft aus und trage
-          Start- und Enddatum ein.
-          <br />
-          Klicke nach dem Erstellen der Einsatzplanung auf "Drucken", um ein PDF zu generieren. Dieses kannst du dann an den Einsatzbetrieb
-          schicken.
-        </p>
-        <p>
-          <b>Beachte:</b> Zivi-Einsätze im Naturschutz müssen an einem Montag beginnen und an einem Freitag enden, ausser es handelt sich um
-          deinen letzten Zivi Einsatz und du leistest nur noch die verbleibenden Resttage.
-        </p>
+        <ServiceSubformExplanationHeader/>
         {user && (
           <div>
             <ServiceOverviewTable
