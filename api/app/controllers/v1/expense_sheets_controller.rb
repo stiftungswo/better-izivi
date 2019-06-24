2ea32782f92a5ad17e9502cef927402bc11cdd2a
Philipp Fehr <philipp@thefehr.me>
2019-06-24 13:21:17 +0200

    Removed obsolete code

--- api/app/controllers/v1/expense_sheets_controller.rb
+++ api/app/controllers/v1/expense_sheets_controller.rb
@@ -57,14 +57,6 @@ module V1
       raise ValidationError, @expense_sheet.errors unless @expense_sheet.destroy
     end
 
-    def export
-      generator = TemplatePdfGeneratorService.new('v1/expense_sheet/index', pdf_locals, 'Landscape')
-      render_pdf(
-        filename: I18n.t('pdfs.phone_list.filename', today: I18n.l(Time.zone.today)),
-        pdf: generator.generate_pdf
-      )
-    end
-
     private
 
     def set_expense_sheet
