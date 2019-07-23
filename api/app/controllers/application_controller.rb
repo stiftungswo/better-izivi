7c1856edf7932dd8a5244be3ff432319a96c8714
Niels Saurer <me@nielssaurer.com>
2019-07-23 15:12:37 +0200

    Add locale param

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -5,5 +5,16 @@ class ApplicationController < ActionController::API
   include Concerns::ErrorHandler
   include Concerns::DeviseUserParamsRegistrable
 
+  PERMITTED_LOCALES = %w[de en fr]
+  
+  before_action :set_locale
+
   respond_to :json
+
+  private
+
+  def set_locale
+    locale = params[:locale]
+    I18n.locale = locale.in?(PERMITTED_LOCALES) ? locale : I18n.default_locale
+  end
 end
