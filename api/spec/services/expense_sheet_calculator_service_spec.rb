f5bf1fe47b7ba48b332228b7ab4c999e2800f2ff
Philipp Fehr <philipp@thefehr.me>
2019-07-29 11:33:12 +0200

    Fix some rubocop errors

--- api/spec/services/expense_sheet_calculator_service_spec.rb
+++ api/spec/services/expense_sheet_calculator_service_spec.rb
@@ -7,22 +7,19 @@ RSpec.describe ExpenseSheetCalculatorService, type: :service do
     before do
       create :service
     end
-    let(:expense_sheet) {  }
+
+    let(:expense_sheet) {}
 
     describe '#calculate_first_day' do
-
     end
   end
 
   context 'when is at the beginning' do
-
   end
 
   context 'when it is at the ending' do
-
   end
 
   context 'when it is at the beginning and ending' do
-
   end
 end
