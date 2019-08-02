75d5a9750db426b5904066045f9f05be65baa4f5
Niels Saurer <me@nielssaurer.com>
2019-08-02 16:53:32 +0200

    Adjust PaymentsController to Payment model

--- api/app/controllers/v1/payments_controller.rb
+++ api/app/controllers/v1/payments_controller.rb
@@ -8,19 +8,13 @@ module V1
     before_action :authenticate_from_params!, if: -> { request.format.xml? }
     before_action :authenticate_user!, unless: -> { request.format.xml? }
     before_action :authorize_admin!
-    before_action :set_unconfirmed_expense_sheets, only: %i[destroy confirm]
 
     def show
-      @sheets = ExpenseSheet.in_payment(payment_timestamp_param)
-
-      raise ActiveRecord::RecordNotFound, I18n.t('payment.errors.not_found') if @sheets.empty?
-
-      payment_timestamp = payment_timestamp_param
-      state = @sheets.first.state
+      @payment = Payment.find(payment_timestamp_param)
 
       respond_to do |format|
         format.json do
-          render :show, locals: { state: state, payment_timestamp: payment_timestamp }
+          render :show
         end
         format.xml do
           render plain: generate_pain, content_type: :xml
@@ -29,39 +23,32 @@ module V1
     end
 
     def index
-      @payments = ExpenseSheet.payment_issued.group_by(&:payment_timestamp)
+      @payments = Payment.all
     end
 
     def create
-      query = ExpenseSheet.includes(:user).ready_for_payment
-      @sheets = query.to_a
+      expense_sheets = ExpenseSheet.includes(:user).ready_for_payment.all
+      @payment = Payment.new(expense_sheets)
 
-      raise ActiveRecord::RecordNotFound, I18n.t('payment.errors.ready_not_found') if query.count.zero?
+      raise ValidationError, @payment.errors unless @payment.save
 
-      payment_timestamp = Time.zone.now
-      state = :payment_in_progress
-
-      query.update state: state, payment_timestamp: payment_timestamp
-
-      render :show, locals: { state: state, payment_timestamp: payment_timestamp }
+      render :show
     end
 
     def destroy
-      payment_timestamp = ''
-      state = :ready_for_payment
+      @payment = Payment.find(payment_timestamp_param)
 
-      unconfirmed_expense_sheets_query.update state: state, payment_timestamp: payment_timestamp
+      raise ValidationError, @payment.errors unless @payment.cancel
 
-      render :show, locals: { state: state, payment_timestamp: payment_timestamp }
+      render :show
     end
 
     def confirm
-      payment_timestamp = payment_timestamp_param
-      state = :paid
+      @payment = Payment.find(payment_timestamp_param)
 
-      unconfirmed_expense_sheets_query.update state: state
+      raise ValidationError, @payment.errors unless @payment.confirm
 
-      render :show, locals: { state: state, payment_timestamp: payment_timestamp }
+      render :show
     end
 
     private
@@ -70,19 +57,8 @@ module V1
       Time.zone.at(params[:payment_timestamp].to_i)
     end
 
-    def set_unconfirmed_expense_sheets
-      @sheets = unconfirmed_expense_sheets_query.to_a
-
-      not_found_exception = ActiveRecord::RecordNotFound.new I18n.t('payment.errors.unconfirmed_not_found')
-      raise not_found_exception if unconfirmed_expense_sheets_query.count.zero?
-    end
-
-    def unconfirmed_expense_sheets_query
-      ExpenseSheet.in_payment(payment_timestamp_param).payment_in_progress
-    end
-
     def generate_pain
-      PainGenerationService.new(@sheets).generate_pain.to_xml('pain.001.001.03.ch.02')
+      PainGenerationService.new(@payment.expense_sheets).generate_pain.to_xml('pain.001.001.03.ch.02')
     end
   end
 end
