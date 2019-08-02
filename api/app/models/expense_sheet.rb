8e3bafe82a2b3b9a57b7b6d5bb89a9bdf1ff8d04
Niels Saurer <me@nielssaurer.com>
2019-08-02 14:25:27 +0200

    Added ExpenseSheet validations

--- api/app/models/expense_sheet.rb
+++ api/app/models/expense_sheet.rb
@@ -23,6 +23,8 @@ class ExpenseSheet < ApplicationRecord
             numericality: { only_integer: true }
 
   validate :legitimate_state_change, if: :state_changed?
+  validates :payment_timestamp, presence: true, if: -> { state.in?(%w[payment_in_progress paid]) }
+  validates :payment_timestamp, inclusion: { in: [nil] }, if: -> { state.in?(%w[open ready_for_payment]) }
 
   enum state: {
     open: 0,
