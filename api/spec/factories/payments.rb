163e9b8e850b314e559138a679fa69d749c5fb84
Niels Saurer <me@nielssaurer.com>
2019-08-06 09:57:59 +0200

    Adjust Payment factory

--- api/spec/factories/payments.rb
+++ api/spec/factories/payments.rb
@@ -4,14 +4,18 @@ FactoryBot.define do
   factory :payment, class: 'Payment' do
     state { :payment_in_progress }
     payment_timestamp { Time.zone.now }
-    expense_sheets do
-      (1..10).map do
-        expense_sheet = build :expense_sheet, state: state, payment_timestamp: payment_timestamp
-        expense_sheet.save(validate: false)
-        expense_sheet
-      end
-    end
-    initialize_with { Payment.new(expense_sheets) }
+    # expense_sheets do
+    #   (1..10).map do
+    #     expense_sheet = build :expense_sheet, state: state, payment_timestamp: payment_timestamp
+    #     expense_sheet.save(validate: false)
+    #     expense_sheet
+    #   end
+    # end
+    initialize_with { Payment.new expense_sheets }
+
+    to_create { |payment| raise ActiveRecord::RecordInvalid unless payment.save }
+
+    after { |payment| payment.expense_sheets.update_all payment_timestamp: payment.payment_timestamp }
   end
 
   trait :paid do
