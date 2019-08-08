b8759e30bb97ce1c501672679297bdfaf8452d42
Niels Saurer <me@nielssaurer.com>
2019-08-08 08:31:20 +0200

    Create new structure

---
+++ api/app/services/expense_sheet_calculators/remaining_days_calculator.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module ExpenseSheetCalculators
+  class RemainingDays
+  end
+end
---
+++ api/app/services/expense_sheet_calculators/suggested_values_calculator.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ExpenseSheetCalculators
+  class Suggestion
+    WORK_CLOTHING_MAX_PER_SERVICE = 24_000
+  end
+end
