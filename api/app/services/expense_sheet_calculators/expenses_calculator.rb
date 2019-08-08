9a04fa6b1f975ad70a870dd53f0159a8fe84030c
Niels Saurer <me@nielssaurer.com>
2019-08-08 11:41:43 +0200

    Add ExpensesCalculator TODO

--- api/app/services/expense_sheet_calculators/expenses_calculator.rb
+++ api/app/services/expense_sheet_calculators/expenses_calculator.rb
@@ -59,6 +59,7 @@ module ExpenseSheetCalculators
       day_sum + calculate_static_expenses
     end
 
+    # TODO: fix this, should also subtract unpaid_company_holiday_days
     def calculate_chargeable_days
       @expense_sheet.duration - @expense_sheet.unpaid_vacation_days
     end
