728274b5f6776ffd387beb8491d90333d139599e
Lukas Bischof <me@luk4s.dev>
2019-08-12 11:56:44 +0200

    Register spec

--- frontend/cypress/integration/register.js
+++
@@ -1,8 +0,0 @@
-describe('registering', function() {
-  it('can register', function() {
-    cy.visit('/');
-    cy.get(':nth-child(1) > .nav-link').click();
-    cy.get('input[type="password"].mt-2').type('password');
-    cy.get('form > [type="button"]').click();
-  });
-});
