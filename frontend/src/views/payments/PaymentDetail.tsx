74f3ef835a9e8771b654862a0556f9b3628b6092
Lukas Bischof <me@luk4s.dev>
2019-08-16 17:08:08 +0200

    Adapt payment detail

--- frontend/src/views/payments/PaymentDetail.tsx
+++ frontend/src/views/payments/PaymentDetail.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
@@ -7,11 +8,12 @@ import IziviContent from '../../layout/IziviContent';
 import { OverviewTable } from '../../layout/OverviewTable';
 import { MainStore } from '../../stores/mainStore';
 import { PaymentStore } from '../../stores/paymentStore';
-import { Column, PaymentExpenseSheet } from '../../types';
-import { ExpenseSheetConfirmer } from './ExpenseSheetConfirmer';
+import { PaymentExpenseSheet } from '../../types';
+import { Formatter } from '../../utilities/formatter';
+import { DownloadIcon } from '../../utilities/Icon';
 
 interface PaymentDetailRouterProps {
-  id?: string;
+  timestamp?: string;
 }
 
 interface Props extends RouteComponentProps<PaymentDetailRouterProps> {
@@ -23,41 +25,37 @@ interface State {
   loading: boolean;
 }
 
+const COLUMNS = [
+  {
+    id: 'zdp',
+    label: 'ZDP',
+    format: ({ user: { zdp } }: PaymentExpenseSheet) => zdp,
+  },
+  {
+    id: 'full_name',
+    label: 'Name',
+    format: (expenseSheet: PaymentExpenseSheet) => (
+      <Link to={'/users/' + expenseSheet.user.id}>{expenseSheet.user.full_name}</Link>
+    ),
+  },
+  {
+    id: 'iban',
+    label: 'IBAN',
+    format: ({ user: { bank_iban } }: PaymentExpenseSheet) => bank_iban,
+  },
+  {
+    id: 'total',
+    label: 'Betrag',
+    format: (expenseSheet: PaymentExpenseSheet) => new Formatter().formatCurrency(expenseSheet.total),
+  },
+];
+
 @inject('mainStore', 'paymentStore')
 @observer
 class PaymentDetailInner extends React.Component<Props, State> {
-  columns: Array<Column<PaymentExpenseSheet>>;
-
   constructor(props: Props) {
     super(props);
-    props.paymentStore!.fetchOne(Number(props.match.params.id)).then(() => this.setState({ loading: false }));
-
-    this.columns = [
-      {
-        id: 'zdp',
-        label: 'ZDP',
-        format: (p: PaymentExpenseSheet) => p.user.zdp,
-      },
-      {
-        id: 'full_name',
-        label: 'Name',
-        format: (expenseSheet: PaymentExpenseSheet) => (
-          <Link to={'/users/' + expenseSheet.user.id}>{expenseSheet.user.full_name}</Link>
-        ),
-      },
-      {
-        id: 'iban',
-        label: 'IBAN',
-        format: (p: PaymentExpenseSheet) => p.user.bank_iban,
-      },
-      {
-        id: 'total',
-        label: 'Betrag',
-        format: (expenseSheet: PaymentExpenseSheet) => (
-          <Link to={'/expense_sheets/' + expenseSheet.id}>{this.props.mainStore!.formatCurrency(expenseSheet.total)}</Link>
-        ),
-      },
-    ];
+    props.paymentStore!.fetchOne(Number(props.match.params.timestamp)).then(() => this.setState({ loading: false }));
 
     this.state = {
       loading: true,
@@ -68,20 +66,25 @@ class PaymentDetailInner extends React.Component<Props, State> {
     const payment = this.props.paymentStore!.payment;
     const title = payment
       ? `Auszahlung vom ${this.props.mainStore!.formatDate(PaymentStore.convertPaymentTimestamp(payment.payment_timestamp))}`
-      : `Details zur Auszahlung ${this.props.match.params.id}`;
+      : `Details zur Auszahlung ${this.props.match.params.timestamp}`;
 
     return (
       <IziviContent card loading={this.state.loading} title={title}>
         {payment && (
           <>
-            <Button color="primary" href={this.props.mainStore!.apiURL(`payments/${payment!.payment_timestamp}.xml`)} tag="a" target="_blank">
-              Zahlung.xml erneut herunterladen
-            </Button>{' '}
-            <br /> <br />
+            <Button
+              color="primary"
+              href={this.props.mainStore!.apiURL(`payments/${payment!.payment_timestamp}.xml`)}
+              tag="a"
+              className="mb-4"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="mr-1" icon={DownloadIcon}/> Zahlungsdatei herunterladen
+            </Button>
             <OverviewTable
-              columns={this.columns}
+              columns={COLUMNS}
               data={this.props.paymentStore!.payment!.expense_sheets}
-              renderActions={(p: PaymentExpenseSheet) => <ExpenseSheetConfirmer paymentExpenseSheet={p} />}
+              renderActions={(expenseSheet: PaymentExpenseSheet) => <Link to={`/expense_sheets/${expenseSheet.id!}`}>Spesenblatt</Link>}
             />
           </>
         )}
