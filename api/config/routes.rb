43143c5f3813f10879d7d433196187f3be871751
Lukas Bischof <me@luk4s.dev>
2019-08-20 13:58:51 +0200

    Fix update

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     devise_scope :user do
       post 'users/validate', to: 'devise_overrides/registrations#validate', defaults: { format: :json }
     end
+
     devise_for :users, defaults: { format: :json }, controllers: {
       registrations: 'devise_overrides/registrations'
     }
@@ -13,17 +14,19 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :regional_centers, only: :index
     resources :holidays, only: %i[index create update destroy]
-    resources :service_specifications, only: %i[index create update]
+    resources :service_specifications, only: %i[index create update], param: :identification_number
+    resources :services
+    resources :users, except: :create
+    resources :payments, except: :update, param: :payment_timestamp
     resources :expense_sheets do
       get 'hints', on: :member
     end
+
     get 'services/calculate_service_days', to: 'service_calculator#calculate_service_days'
     get 'services/calculate_ending', to: 'service_calculator#calculate_ending'
-    resources :services
-    resources :users, except: :create
-    put 'payments/:payment_timestamp/confirm', to: 'payments#confirm', as: 'payment_confirm'
-    resources :payments, except: :update, param: :payment_timestamp
     get 'phone_list', to: 'phone_list#show', as: 'phone_list_export'
     get 'expense_sheet', to: 'expense_sheets#show', as: 'expense_sheet_export'
+
+    put 'payments/:payment_timestamp/confirm', to: 'payments#confirm', as: 'payment_confirm'
   end
 end
