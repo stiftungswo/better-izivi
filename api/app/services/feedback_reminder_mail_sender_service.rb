77c2061d6ba61c84e272d928d1f47a7ab80daefb
Lukas Bischof <me@luk4s.dev>
2019-08-20 10:52:20 +0200

    Rake task

--- api/app/services/feedback_reminder_mail_sender_service.rb
+++ api/app/services/feedback_reminder_mail_sender_service.rb
@@ -2,11 +2,20 @@
 
 class FeedbackReminderMailSenderService
   def send_reminders
-    services = Service.where(feedback_mail_sent: false).where(Service.arel_table[:ending].lt(Time.zone.now))
+    Service
+      .where(feedback_mail_sent: false)
+      .where(Service.arel_table[:ending].lt(Time.zone.now))
+      .each(&method(:send_email))
+  end
 
-    services.each do |service|
-      FeedbackMailer.feedback_reminder_mail(service).deliver_now
-      service.update feedback_mail_sent: true
-    end
+  private
+
+  def send_email(service)
+    FeedbackMailer.feedback_reminder_mail(service).deliver_now
+    service.update feedback_mail_sent: true
+
+    # rubocop:disable Rails/Output
+    puts "Sent to #{service.user.email} (Service id ##{service.id})"
+    # rubocop:enable Rails/Output
   end
 end
