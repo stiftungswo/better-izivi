2b0a2203aa21f0d7241038c06b1952501376dbad
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2019-10-08 11:49:20 +0200

    Added expenses_overview controller & service

---
+++ api/app/controllers/v1/expenses_overview_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module V1
+  class ExpensesOverviewController < FileController
+    include V1::Concerns::AdminAuthorizable
+
+    before_action :authorize_admin!
+    before_action :load_specifications, only: :show
+
+    def show
+      respond_to do |format|
+        format.pdf do
+          pdf = Pdfs::ExpensesOverviewService.new(@specifications, sanitized_filters)
+
+          send_data pdf.render,
+                    filename: I18n.t('pdfs.expenses_overview.filename', today: I18n.l(Time.zone.today)),
+                    type: 'application/pdf',
+                    disposition: 'inline'
+        end
+      end
+    end
+
+    private
+
+    def load_specifications
+      @specifications = Service.overlapping_date_range(sanitized_filters.beginning, sanitized_filters.ending)
+                               .includes(:service_specification, :user)
+                               .order('service_specification_id')
+                               .group_by { |service| service.service_specification.name }
+    end
+
+    # :reek:FeatureEnvy
+    def filter_params
+      params.require(:expenses_overview).permit(:beginning, :ending).tap do |expenses_overview_params|
+        expenses_overview_params.require(:beginning)
+        expenses_overview_params.require(:ending)
+      end
+    end
+
+    def sanitized_filters
+      @sanitized_filters ||= OpenStruct.new(
+        beginning: Date.parse(filter_params[:beginning]), 
+        ending: Date.parse(filter_params[:ending])
+      )
+    end
+  end
+end
