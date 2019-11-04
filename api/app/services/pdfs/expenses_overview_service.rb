bc9ff65afee75de23e854e6c14fbaa3dacb9243e
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2019-11-04 16:37:06 +0100

    fixed errors

--- api/app/services/pdfs/expenses_overview_service.rb
+++ api/app/services/pdfs/expenses_overview_service.rb
@@ -28,25 +28,15 @@ module Pdfs
     def headline
       text I18n.t('pdfs.expenses_overview.swo', date: I18n.l(Time.zone.today)), align: :right, size: 8
       text I18n.t('pdfs.expenses_overview.basedon', date: I18n.l(Time.zone.today)), align: :right, size: 8
-      text(
-        I18n.t(
-          'pdfs.expenses_overview.title',
-          beginning: I18n.l(@beginning),
-          ending: I18n.l(@ending)
-        ),
-        align: :left,
-        style: :bold,
-        size: 15
-      )
+      text(I18n.t('pdfs.expenses_overview.title', beginning: I18n.l(@beginning), ending: I18n.l(@ending)),
+           align: :left, style: :bold, size: 15)
     end
 
     def header
       font_size 9
       table([Pdfs::ExpensesOverview::ExpensesOverviewAdditions::TABLE_HEADER,
              Pdfs::ExpensesOverview::ExpensesOverviewAdditions::TABLE_SUB_HEADER],
-            cell_style: { borders: [] },
-            width: bounds.width,
-            header: true,
+            cell_style: { borders: [] }, width: bounds.width, header: true,
             column_widths: Pdfs::ExpensesOverview::ExpensesOverviewAdditions::COLUMN_WIDTHS) do
         row(0).font_style = :bold
       end
@@ -55,10 +45,9 @@ module Pdfs
     def content_table
       font_size 9
       @service_specifications.values.each do |expense_sheets|
-        table(table_data(expense_sheets),
-              cell_style: { borders: [] },
-              width: bounds.width, header: true,
-              column_widths: Pdfs::ExpensesOverview::ExpensesOverviewAdditions::COLUMN_WIDTHS)
+        # rubocop:disable Metrics/LineLength
+        table(table_data(expense_sheets), cell_style: { borders: [] }, width: bounds.width, header: true, column_widths: Pdfs::ExpensesOverview::ExpensesOverviewAdditions::COLUMN_WIDTHS)
+        # rubocop:enable Metrics/LineLength
         sum_table(expense_sheets)
       end
     end
@@ -67,9 +56,9 @@ module Pdfs
       table([[{ content: 'Gesamt: ', align: :left },
               { content: (expense_sheets.sum(&:work_days) + expense_sheets.sum(&:workfree_days)).to_s,
                 align: :right },
-              { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheets.sum(&:calculate_full_expenses).to_s),
-                align: :right }]], cell_style: { borders: %i[] },
-                                   header: false, position: :right, column_widths: [40, 30, 45]) do
+              # rubocop:disable Metrics/LineLength
+              { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheets.sum(&:calculate_full_expenses).to_s), align: :right }]], cell_style: { borders: %i[] }, header: false, position: :right, column_widths: [40, 30, 45]) do
+        # rubocop:enable Metrics/LineLength
         row(0).font_style = :bold
       end
     end
@@ -92,12 +81,9 @@ module Pdfs
     end
 
     def method2(expense_sheet)
-      [{ content: expense_sheet.work_days.to_s, align: :right },
-       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-         expense_sheet.calculate_work_days[:total] +
-         expense_sheet.calculate_first_day[:total] +
-         expense_sheet.calculate_last_day[:total]
-       ), align: :right },
+      # rubocop:disable Metrics/LineLength
+      [{ content: expense_sheet.work_days.to_s, align: :right }, { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_work_days[:total] + expense_sheet.calculate_first_day[:total] + expense_sheet.calculate_last_day[:total]), align: :right },
+       # rubocop:enable Metrics/LineLength
        { content: expense_sheet.workfree_days.to_s, align: :right }]
     end
 
@@ -106,9 +92,7 @@ module Pdfs
         expense_sheet.calculate_workfree_days[:total]
       ), align: :right },
        { content: expense_sheet.sick_days.to_s, align: :right },
-       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-         expense_sheet.calculate_sick_days[:total]
-       ), align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_sick_days[:total]), align: :right },
        { content: expense_sheet.paid_vacation_days.to_s, align: :right },
        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
          expense_sheet.calculate_paid_vacation_days[:total]
@@ -117,46 +101,32 @@ module Pdfs
 
     def method4(expense_sheet)
       [{ content: expense_sheet.unpaid_vacation_days.to_s, align: :right },
-       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-         expense_sheet.calculate_unpaid_vacation_days[:total]
-       ), align: :right },
-       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-         expense_sheet.driving_expenses
-       ), align: :right },
-       { content: (expense_sheet.work_days +
-         expense_sheet.workfree_days).to_s, align: :right }]
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_unpaid_vacation_days[:total]),
+         align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.driving_expenses), align: :right },
+       { content: (expense_sheet.work_days + expense_sheet.workfree_days).to_s, align: :right }]
     end
 
     def method5(expense_sheet)
       [
-        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-          expense_sheet.clothing_expenses
-        ), align: :right },
-        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-          expense_sheet.extraordinary_expenses
-        ), align: :right },
-        { content: (expense_sheet.work_days +
-          expense_sheet.workfree_days).to_s, align: :right }
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.clothing_expenses), align: :right },
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.extraordinary_expenses), align: :right },
+        { content: (expense_sheet.work_days + expense_sheet.workfree_days).to_s, align: :right }
       ]
     end
 
     def method6(expense_sheet)
       [
-        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
-          expense_sheet.calculate_full_expenses.to_d
-        ), align: :right }
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_full_expenses.to_d), align: :right }
       ]
     end
 
     def table_content(expense_sheets)
       expense_sheets.map do |expense_sheet|
         expense_sheet.slice.values
-        method1(expense_sheet) +
-          method2(expense_sheet) +
-          method3(expense_sheet) +
-          method4(expense_sheet) +
-          method5(expense_sheet) +
-          method6(expense_sheet)
+        # rubocop:disable Metrics/LineLength
+        method1(expense_sheet) + method2(expense_sheet) + method3(expense_sheet) + method4(expense_sheet) + method5(expense_sheet) + method6(expense_sheet)
+        # rubocop:enable Metrics/LineLength
       end
     end
   end
