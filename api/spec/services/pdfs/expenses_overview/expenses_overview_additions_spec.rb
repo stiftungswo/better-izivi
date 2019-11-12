55f34ff4db609d7d01582bcf783a37b0aa64a1e3
Sebi <sebastianbrunner@hotmail.ch>
2019-11-12 10:49:58 +0100

    added addditions testclass

---
+++ api/spec/services/pdfs/expenses_overview/expenses_overview_additions_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Pdfs::ExpensesOverview::ExpensesOverviewAdditions, type: :service do
+  let(:service) { create :service }
+end
