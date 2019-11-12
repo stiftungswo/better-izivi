fe2cdcdf782e694af79af9e1775449b2fcfc60e3
Sebi <sebastianbrunner@hotmail.ch>
2019-11-12 14:15:17 +0100

    changed test

--- api/spec/services/pdfs/expenses_overview_service_spec.rb
+++ api/spec/services/pdfs/expenses_overview_service_spec.rb
@@ -3,5 +3,49 @@
 require 'rails_helper'
 
 RSpec.describe Pdfs::ExpensesOverviewService, type: :service do
-  let(:service) { create :service }
+  describe '#render' do
+    context 'when locale is german' do
+      before do
+        I18n.locale = :de
+        create(:service, service_data)
+      end
+
+      after { I18n.locale = I18n.default_locale }
+
+      let(:pdf) { described_class.new(service_specifications, phone_list_dates).render }
+      let(:user) { create :user }
+      let(:dates) do
+        OpenStruct.new(
+          beginning: Date.parse('2017-01-01'),
+          ending: Date.parse('2018-12-31')
+        )
+      end
+
+      let(:service_specifications) do
+        [Service.includes(:service_specification, :user).first]
+          .group_by { |service| service.service_specification.name }
+      end
+
+      let(:service_data) do
+        {
+          beginning: Date.parse('2018-01-01'),
+          ending: Date.parse('2018-02-23'),
+          user: user
+        }
+      end
+
+      let(:pdf_text_inspector) { PDF::Inspector::Text.analyze(pdf) }
+      let(:pdf_page_inspector) { PDF::Inspector::Page.analyze(pdf) }
+
+      let(:expected_texts) do
+        [
+          I18n.t('pdfs.phone_list.header', date: I18n.l(Time.zone.today)),
+          'Telefonliste vom 01.01.2017 bis 31.12.2018',
+          'MyServiceSpecification',
+          'Nachname', 'Vorname', 'Adresse', 'PLZ / Ort', 'Telefonnummer', 'Email',
+          'Zivi', 'Mustermann', 'Bahnstrasse 18b', '8603 Schwerzenbach', '+41 (0) 76 123 45 67', user.email
+        ]
+      end
+    end
+  end
 end
