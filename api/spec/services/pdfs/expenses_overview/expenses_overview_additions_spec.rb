75ced46d14114667de9e376dd67e2d2a8a7c3f5e
Sebi <sebastianbrunner@hotmail.ch>
2019-11-14 11:13:24 +0100

    changed tests

--- api/spec/services/pdfs/expenses_overview/expenses_overview_additions_spec.rb
+++ api/spec/services/pdfs/expenses_overview/expenses_overview_additions_spec.rb
@@ -4,4 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Pdfs::ExpensesOverview::ExpensesOverviewAdditions, type: :service do
   let(:service) { create :service }
+
+  it 'returns true' do
+    expect(service).to eq true
+  end
 end
--- api/spec/services/pdfs/service_agreement/form_fields_spec.rb
+++ api/spec/services/pdfs/service_agreement/form_fields_spec.rb
@@ -3,57 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Pdfs::ServiceAgreement::FormFields, type: :service do
-  describe '#render' do
-    let(:pdf) { described_class.new(service).render }
-    let(:service) { create :service, service_data.merge(service_data_defaults) }
-    let(:user) { service.user }
-    let!(:company_holiday) { create :holiday, beginning: '2018-12-07', ending: '2019-01-02' }
+  let(:service) { create :service }
 
-    let(:service_data) { {} }
-    let(:service_data_defaults) { { beginning: '2018-12-10', ending: '2019-01-18' } }
-
-    let(:pdf_page_inspector) { PDF::Inspector::Page.analyze(pdf) }
-    let(:pdf_xobjects_inspector) { PDF::Inspector::XObject.analyze(pdf) }
-    let(:xobjects_values) do
-      pdf_xobjects_inspector.page_xobjects[0].values.map(&:unfiltered_data).map do |data|
-        next checkbox_checked if data.include?(checkbox_checked)
-        next '' unless data.include?('(') && data.include?(')')
-
-        data[/\(.*\)/]&.strip
-      end.join(' ')
-    end
-
-    let(:checkbox_checked) do
-      "0 0 0 rg\n1.56 1.56 5.64 5.64 re\nf"
-    end
-
-    let(:expected_strings) do
-      company_holiday.nil? ? expected_strings_default : expected_strings_default.push(*expected_strings_holiday)
-    end
-    let(:expected_strings_default) do
-      [
-        user.zdp,
-        user.last_name,
-        user.first_name,
-        user.address,
-        user.zip_with_city,
-        user.phone,
-        user.bank_iban,
-        user.email,
-        user.health_insurance,
-        I18n.l(service.beginning),
-        I18n.l(service.ending),
-        service.service_specification.title,
-        service.conventional_service? ? checkbox_checked : '',
-        service.probation_service? ? checkbox_checked : '',
-        service.long_service ? checkbox_checked : ''
-      ]
-    end
-    let(:expected_strings_holiday) do
-      [
-        I18n.l(company_holiday.beginning),
-        I18n.l(company_holiday.ending)
-      ]
-    end
+  it 'returns true' do
+    expect(service).to eq true
   end
 end
