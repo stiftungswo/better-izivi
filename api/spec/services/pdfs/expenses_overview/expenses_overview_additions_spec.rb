e25010e0da706401a605c3fe01bb1fc36997ac27
Sebi <sebastianbrunner@hotmail.ch>
2019-11-15 08:44:53 +0100

    changed tests

--- api/spec/services/pdfs/expenses_overview/expenses_overview_additions_spec.rb
+++ api/spec/services/pdfs/expenses_overview/expenses_overview_additions_spec.rb
@@ -2,10 +2,18 @@
 
 require 'rails_helper'
 
-RSpec.describe Pdfs::ExpensesOverview::ExpensesOverviewAdditions, type: :service do
-  let(:service) { create :service }
+RSpec.describe Pdfs::ExpensesOverview::ExpensesOverviewAdditions do
+  let(:additions) { described_class.new }
 
-  it 'returns true' do
-    expect(service).to eq true
+  it 'defines a TABLE_SUB_HEADER' do
+    expect(additions::TABLE_SUB_HEADER).not_to eq null
+  end
+
+  it 'defines a TABLE_HEADER' do
+    expect(additions::TABLE_HEADER).not_to eq null
+  end
+
+  it 'defines the COLOR_GREY' do
+    expect(additions::COLOR_GREY).not_to eq null
   end
 end
