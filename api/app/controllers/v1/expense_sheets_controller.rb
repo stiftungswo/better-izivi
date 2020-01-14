b4fca75ad108c6dc4396c20d7333d8db98e0eaa2
gumbee <mugeeb.hassan@gmail.com>
2020-01-14 09:25:28 +0100

    order by oldest first

--- api/app/controllers/v1/expense_sheets_controller.rb
+++ api/app/controllers/v1/expense_sheets_controller.rb
@@ -23,7 +23,7 @@ module V1
     ].freeze
 
     def index
-      @expense_sheets = filtered_expense_sheets.order(beginning: :desc, ending: :desc)
+      @expense_sheets = filtered_expense_sheets.order(beginning: :asc, ending: :asc)
     end
 
     def show
