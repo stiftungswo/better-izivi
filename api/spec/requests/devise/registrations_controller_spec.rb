283631d309ed65065928d4fd3a00b947b6a64c3c
Jonathan Stahl <jonathan.stahl@uzh.ch>
2020-08-24 13:26:30 +0200

    Revert "Ugly fix for activerecord attributes translations test"
    This reverts commit 38d5d44f364662b614700e22185d6308ae21567e.

--- api/spec/requests/devise/registrations_controller_spec.rb
+++ api/spec/requests/devise/registrations_controller_spec.rb
@@ -69,10 +69,8 @@ RSpec.describe Devise::RegistrationsController, type: :request do
           first_name: [I18n.t('errors.messages.blank')],
           last_name: [I18n.t('errors.messages.blank')]
         )
-        # TODO: activerecord translations like first_name and last_name seem to default back to english,
-        # because of the izivi.backend key.
-        expect(response_json[:human_readable_descriptions]).to contain_exactly('First name muss ausgefüllt werden',
-                                                                               'Last name muss ausgefüllt werden')
+        expect(response_json[:human_readable_descriptions]).to contain_exactly('Vorname muss ausgefüllt werden',
+                                                                               'Nachname muss ausgefüllt werden')
       end
     end
 
