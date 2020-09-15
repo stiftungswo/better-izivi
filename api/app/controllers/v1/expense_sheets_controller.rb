8ab907a242cc8023d66e1c94e09a1581bbdeae9a
Gautham Arumugam <gaut-3@hotmail.com>
2020-09-15 16:14:14 +0200

    Allow zivis to download their expense sheets. Currently it is not working. #357

--- api/app/controllers/v1/expense_sheets_controller.rb
+++ api/app/controllers/v1/expense_sheets_controller.rb
@@ -8,9 +8,9 @@ module V1
 
     before_action :authenticate_user!, unless: -> { request.format.pdf? }
     before_action :authenticate_from_params!, if: -> { request.format.pdf? }
-    before_action :set_expense_sheet, only: %i[show update destroy hints]
+    before_action :set_expense_sheet, only: %i[show update destroy hints create]
     before_action :set_service, only: :create
-    before_action :protect_foreign_resource!, except: %i[index create], unless: -> { current_user.admin? }
+    before_action :protect_foreign_resource!, except: %i[index], unless: -> { current_user.admin? }
     before_action :authorize_admin!, only: %i[index]
 
     PERMITTED_EXPENSE_SHEET_KEYS = %i[
