998e93d3ca9015017749a25d5dbd8ebaccd6fac6
ildacheu <rewoperrepower@gmail.com>
2021-01-20 09:15:02 +0100

    code reformat

--- api/app/services/subscribe_to_newsletter.rb
+++ api/app/services/subscribe_to_newsletter.rb
@@ -1,12 +1,14 @@
-class Subscribe_to_newsletter
+# frozen_string_literal: true
+
+class SubscribeToNewsletter
   def initialize(params)
     @body = { email: params['user']['email'],
               first_name: params['user']['first_name'],
               last_name: params['user']['last_name'] }.to_json
-    make_post()
+    make_post
   end
 
-  def make_post()
+  def make_post
     uri = URI('https://www.stiftungswo.ch/wp-json/newsletter/v2/subscribers')
     req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
     req.basic_auth ENV['NEWSLETTER_API_CLIENT_KEY'], ENV['NEWSLETTER_API_CLIENT_SECRET']
@@ -14,6 +16,6 @@ class Subscribe_to_newsletter
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
     req.body = @body
-    print http.request(req).body
+    http.request(req).
   end
 end
