d25286e59be6347e970392216c338c43555926ee
Maurus Fry <40545498+ildacheu@users.noreply.github.com>
2021-01-25 10:11:35 +0100

    Update PersonalDetailsPage.tsx

--- frontend/src/views/register/pages/PersonalDetailsPage.tsx
+++ frontend/src/views/register/pages/PersonalDetailsPage.tsx
@@ -184,17 +184,17 @@ export class PersonalDetailsPage extends React.Component<PersonalDetailsPageProp
         <WiredField
           component={CheckboxField}
           name={'newsletter'}
-          label={//<span><a href="https://www.stiftungswo.ch/datenschutzerklaerung/">Datenschutzerklärung</a>
+          label={
              this.intl.formatMessage({
                id: 'register.personalDetailsPage.newsletter_yes',
                defaultMessage: 'Ja, ich möchte den SWO Newsletter erhalten',
              },
              {
-               link: <a href="https://www.stiftungswo.ch/datenschutzerklaerung/">Datenschutzerklärung</a>
+               link_de: <a href="https://www.stiftungswo.ch/datenschutzerklaerung/">Datenschutzerklärung</a>,
+               link_fr: <a href="https://www.stiftungswo.ch/datenschutzerklaerung/">charte de confidentialité</a>,
+               link_en: <a href="https://www.stiftungswo.ch/datenschutzerklaerung/">privacy policy</a>
              })
-
-           //</span>
-         }
+           }
         />
       </>
     );
