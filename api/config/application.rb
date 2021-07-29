8ae05642cbffd3db3de6a853956c290729900645
Lukas Bischof <me@luk4s.dev>
2021-07-29 11:32:51 +0200

    Add session

--- api/config/application.rb
+++ api/config/application.rb
@@ -31,6 +31,9 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
 
     config.action_mailer.default_url_options = {
       host: ENV.fetch('APP_HOST', 'localhost'),
