187a67068841cfbedf5f6527643affac0bf77b18
Lukas Bischof <me@luk4s.dev>
2021-07-29 09:49:32 +0200

    Introduce Sidekiq

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -53,8 +53,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "api_#{Rails.env}"
+  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_name_prefix = "izivi_api_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
