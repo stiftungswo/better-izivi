d3b53d2a0a382ee0543254a7535b3755ffd28d62
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2021-11-18 16:09:31 +0100

    remove http status code spec tests

--- api/spec/routing/v1/payments_list_routing_spec.rb
+++ api/spec/routing/v1/payments_list_routing_spec.rb
@@ -7,13 +7,5 @@ RSpec.describe V1::PaymentsListController, type: :routing do
     it 'routes to #show' do
       expect(get: '/v1/payments_list.pdf').to route_to('v1/payments_list#show', format: 'pdf')
     end
-
-    it 'returns 400 if the token parameter is missing' do
-      expect(get: '/v1/payments_list.pdf?locale=de').to have_status(200)
-    end
-
-    it 'returns 401 if the token is incorrect' do
-      expect(get: '/v1/payments_list.pdf?locale=de&token=meow').to have_status(401)
-    end
   end
 end
